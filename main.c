597fbd26d0e11b91f487b9f4b0e13df219b741c1
dineend <dineend@mcmaster.ca>
2020-05-06 19:52:50 -0400

    Altered main.c with average from 1-7

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 
 
 int main() {
-    double arr[] = {1.0, 2.0, 3.0, 4.0};
+    double arr[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
 
-    double result = average(4, arr);
+    double result = average(7, arr);
 
-    printf("The average of 1, 2, 3 and 4 is: %.4f\n", result);
+    printf("The average of 1-7 is: %.4f\n", result);
     return 0;    
 }
 
